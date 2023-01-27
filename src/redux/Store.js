5040f9767675698c3982fc820f0f8edd625a3ef5
VanHao1-2-3 <hovanhao12b1@gmail.com>
2023-01-27 20:52:27 +0700

    first commit

--- src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlices from "../Slices/cartSlices";
-const store = configureStore({
-   reducer:{
-      cart: cartSlices
-   }
-})
-export default store
